9210c7acadabfb6c207108bf72045abbc8581f89
Mario Fernandez S <mefernandez@unimagdalena.edu.co>
2023-02-04 21:51:43 -0500

    update package.json,index.js, index.html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
-import Helmet from "react-helmet"
 import { Link, graphql } from "gatsby"
+import Helmet from "react-helmet"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
@@ -25,7 +25,6 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
       <Helmet>
       <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Helmet>
